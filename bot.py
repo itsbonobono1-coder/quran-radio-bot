aa01ade1ee8fcf1ed585d66d307f0d0bc27a43e2
itsbonobono1-coder <itsbonobono1@gmail.com>
2026-06-20 15:42:51 +0300

    Enhance start command with inline keyboard options
    Added inline keyboard options for reader selection in the start command.

--- bot.py
+++ bot.py
@@ -1,10 +1,20 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "8489184890:AAHLrgKqEL3wVwPwduQpV3V-_g5aQC5aPk8"
+TOKEN = "حط التوكن الجديد هنا"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("البوت شغال ✅")
+
+    keyboard = [
+        [InlineKeyboardButton("محمد اللحيدان", callback_data="luhaidan")],
+        [InlineKeyboardButton("ماهر المعيقلي", callback_data="muaiqly")],
+        [InlineKeyboardButton("ياسر الدوسري", callback_data="dosary")],
+    ]
+
+    await update.message.reply_text(
+        "اختر القارئ:",
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
 
 app = Application.builder().token(TOKEN).build()
 
