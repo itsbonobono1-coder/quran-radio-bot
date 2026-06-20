d6c225dda00fe39e09927a3e3e2de2a4ab3fb0fd
itsbonobono1-coder <itsbonobono1@gmail.com>
2026-06-20 17:39:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,37 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
-Application,
-CommandHandler,
-CallbackQueryHandler,
-ContextTypes
+    Application,
+    CommandHandler,
+    CallbackQueryHandler,
+    ContextTypes
 )
 
 TOKEN = "8958924076:AAGmNgBxMsOIqQ_MZxYNfZQIuQv0dBpAg1M"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-keyboard = [
-[InlineKeyboardButton("محمد اللحيدان", callback_data="luhaidan")],
-[InlineKeyboardButton("ماهر المعيقلي", callback_data="muaiqly")],
-[InlineKeyboardButton("ياسر الدوسري", callback_data="dosary")]
-]
+    keyboard = [
+        [InlineKeyboardButton("محمد اللحيدان", callback_data="luhaidan")],
+        [InlineKeyboardButton("ماهر المعيقلي", callback_data="muaiqly")],
+        [InlineKeyboardButton("ياسر الدوسري", callback_data="dosary")]
+    ]
 
-await update.message.reply_text(
-    "اختر القارئ:",
-    reply_markup=InlineKeyboardMarkup(keyboard)
-)
+    await update.message.reply_text(
+        "اختر القارئ:",
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
 
 async def button_click(update: Update, context: ContextTypes.DEFAULT_TYPE):
-query = update.callback_query
-await query.answer()
+    query = update.callback_query
+    await query.answer()
 
-if query.data == "luhaidan":
-    await query.message.reply_text("تم اختيار محمد اللحيدان")
+    if query.data == "luhaidan":
+        await query.message.reply_text("تم اختيار محمد اللحيدان")
 
-elif query.data == "muaiqly":
-    await query.message.reply_text("تم اختيار ماهر المعيقلي")
+    elif query.data == "muaiqly":
+        await query.message.reply_text("تم اختيار ماهر المعيقلي")
 
-elif query.data == "dosary":
-    await query.message.reply_text("تم اختيار ياسر الدوسري")
+    elif query.data == "dosary":
+        await query.message.reply_text("تم اختيار ياسر الدوسري")
 
 app = Application.builder().token(TOKEN).build()
 
